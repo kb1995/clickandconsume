11258e7d39ce1a8c5e6e152e07608e1b2edf5431
Kristiyan Bogdanov <kristiyan.r.bogdanov@gmail.com>
2018-09-28 16:06:16 +0100

    basic video playing full screen

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {} from './Video.styled'
+import ReactPlayer from 'react-player'
 
 class Video extends Component {
   render() {
     return (
-      <div>
-        <h1>This will be the video</h1>
-      </div>
+      <React.Fragment>
+        <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' playing width="100vw" height="100vh" />
+
+      </React.Fragment>
     );
   }
 }
---
+++ src/theme/globalStyle.jsx
@@ -0,0 +1,11 @@
+import { injectGlobal } from "styled-components";
+
+injectGlobal`
+    @import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: 'Roboto Mono', monospace;
+        font-size: 12px;
+    };
+`;
