0f0bbef31c9b9b3e7fe12daf9ae4136a49f8fac2
Kristiyan Bogdanov <kristiyan.r.bogdanov@gmail.com>
2018-09-28 15:51:17 +0100

    basic component

---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {} from './Video.styled'
+
+class Video extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This will be the video</h1>
+      </div>
+    );
+  }
+}
+
+export default Video;
